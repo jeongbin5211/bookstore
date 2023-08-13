ff5f6d3ba222ed40205c3559ae4dd2af366499ed
jeongbin5211 <hjn123493@gmail.com>
2023-08-14 06:48:35 +0900

    0814

---
+++ bookstore/src/mocks/mockData.ts
@@ -0,0 +1,23 @@
+export const mockAskData = [
+  {
+    askId: 1,
+    askSort: '제품 문의',
+    askTitle: '제품 문의입니다.',
+    askStatus: '문의 접수',
+    askDatetime: '2023-08-01',
+  },
+  {
+    askId: 2,
+    askSort: '주문/결제 문의',
+    askTitle: '주문과 결제에 대해 문의합니다.',
+    askStatus: '답변 완료',
+    askDatetime: '2023-07-01',
+  },
+  {
+    askId: 3,
+    askSort: '교환/취소 문의',
+    askTitle: '교환과 취소에 대해 문의합니다.',
+    askStatus: '답변 완료',
+    askDatetime: '2023-06-01',
+  },
+]
